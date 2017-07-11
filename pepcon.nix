dd56a4e2f88a9fe0357f332fbc8d52bac17685b0
Alex Crawford <alex@acrawford.com>
2017-07-10 21:40:18 -0700

    pepcon: numerous updates

--- pepcon.nix
+++ pepcon.nix
@@ -5,11 +5,10 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader.grub = {
-    enable  = true;
-    version = 2;
-    device = "/dev/vda";
-    timeout = 1;
+  boot.loader = {
+    systemd-boot.enable      = true;
+    efi.canTouchEfiVariables = true;
+    timeout                  = 1;
   };
 
   networking.hostName = "pepcon";
@@ -25,48 +24,52 @@
   security.sudo.wheelNeedsPassword = false;
 
   environment.systemPackages = with pkgs; [
-    wget
-    vim
-    rustc
+    (import ./vim_config.nix)
+    file
+    gcc
+    gcc-arm-embedded
+    gnumake
+    git
+    patchelf
     tmux
-    xorg.xmodmap
+    usbutils
+    vim
+    wget
     zsh
   ];
 
-  environment.shells = [
-    "${pkgs.zsh}/bin/zsh"
-  ];
+  services = {
+    sshd.enable = true;
 
-  services.xserver = {
-    enable     = true;
-    layout     = "us";
-    xkbVariant = "dvp";
-    xkbOptions = "ctrl:nocaps";
+    xserver = {
+      enable     = true;
+      layout     = "us";
+      xkbVariant = "dvp";
+      xkbOptions = "ctrl:nocaps";
 
-    desktopManager = {
-      default      = "none";
-      xterm.enable = false;
+      desktopManager.xterm.enable = false;
+
+      displayManager.slim = {
+        enable      = true;
+        defaultUser = "alex";
+        autoLogin   = true;
+      };
+
+      windowManager = {
+        awesome.enable = true;
+        default        = "awesome";
+      };
     };
 
-    displayManager.slim = {
-      enable      = true;
-      defaultUser = "alex";
-      autoLogin   = true;
-    };
-
-    windowManager = {
-      awesome.enable = true;
-      default        = "awesome";
-    };
+    udev.extraRules = ''
+      ATTR{idVendor}=="1366", ATTR{idProduct}=="1010", MODE="0666"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f001", MODE="0666"
+    '';
   };
 
   users.extraUsers.alex = {
-    name       = "alex";
-    group      = "wheel";
-    createHome = true;
-    home       = "/home/alex";
-    shell      = "${pkgs.zsh}/bin/zsh";
+    isNormalUser = true;
+    extraGroups  = [ "wheel" ];
+    shell        = "${pkgs.zsh}/bin/zsh";
   };
-
-  system.stateVersion = "15.09";
 }
