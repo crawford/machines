6cc994651cb7a86607b9893022799dfff8e410af
Alex Crawford <machines@code.acrawford.com>
2020-04-21 21:48:55 -0700

    pepcon: make use of common config

--- pepcon.nix
+++ pepcon.nix
@@ -1,22 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./common.nix ];
 
-  nix = {
-    autoOptimiseStore = true;
-    buildCores        = 8;
-    useSandbox        = true;
-    gc.automatic = true;
-  };
+  nix.maxJobs = 4;
 
-  nixpkgs.config = {
-    allowUnfree                    = true;
-    vim.ftNixSupport               = true;
-    virtualbox.enableExtensionPack = true;
-  };
+  nixpkgs.config.virtualbox.enableExtensionPack = true;
 
   boot.loader = {
     systemd-boot.enable      = true;
@@ -26,94 +15,32 @@
 
   networking.hostName = "pepcon";
 
-  i18n = {
-    consoleFont   = "Lat2-Terminus16";
-    consoleKeyMap = "dvorak";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "US/Pacific";
-
-  security.sudo.wheelNeedsPassword = false;
-
-  environment = {
-    shells = [ "${pkgs.zsh}/bin/zsh" ];
-
-    sessionVariables = {
-      EDITOR = "vim";
-    };
-
-    systemPackages = with pkgs; [
-      (import ./vim-config.nix)
-      tmux
-      zsh
-    ];
-  };
-
-  programs = {
-    tmux = {
-      enable = true;
-      clock24 = true;
-      keyMode = "vi";
-      historyLimit = 50000;
-      terminal = "screen-256color";
-      extraTmuxConf = ''
-        set-option -g status-bg black
-        set-option -g status-fg white
-        set-window-option -g window-status-current-bg black
-        set-window-option -g window-status-current-fg cyan
-      '';
-    };
-
-    zsh = {
-      enable = true;
-      interactiveShellInit = ''
-        cat << EOF > $HOME/.zshrc
-        source ${import ./zsh-config.nix}
-        EOF
-      '';
-    };
-  };
-
   services = {
-    xserver = {
-      enable     = true;
-      layout     = "us";
-      xkbVariant = "dvp";
-      xkbOptions = "ctrl:nocaps";
-
-      desktopManager.xterm.enable = false;
-
-      displayManager.slim = {
-        enable      = true;
-        defaultUser = "alex";
-        autoLogin   = true;
-      };
-
-      windowManager = {
-        awesome.enable = true;
-        default        = "awesome";
-      };
-    };
-
     udev.extraRules = ''
       ATTR{idVendor}=="1366", ATTR{idProduct}=="1010", MODE="0666"
       ATTR{idVendor}=="1366", ATTR{idProduct}=="1015", MODE="0666"
       KERNEL=="hidraw*", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f001", MODE="0666"
     '';
 
-    openssh = {
-      enable = true;
-      passwordAuthentication = false;
-      permitRootLogin = "no";
+    xserver = {
+      desktopManager.xterm.enable  = false;
+      windowManager.awesome.enable = true;
+
+      displayManager = {
+        defaultSession = "none+awesome";
+
+        lightdm = {
+          enable         = true;
+          greeter.enable = true;
+
+          autoLogin = {
+            enable = true;
+            user   = "alex";
+          };
+        };
+      };
     };
-
-    sshguard.enable = true;
   };
 
-  users.extraUsers.alex = {
-    isNormalUser = true;
-    extraGroups  = [ "wheel" ];
-    shell        = "${pkgs.zsh}/bin/zsh";
-  };
+  system.stateVersion = "20.03";
 }
