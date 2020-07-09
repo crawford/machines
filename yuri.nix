12c5961ce77ac491e57dc4008975711956022500
Alex Crawford <crawford@redhat.com>
2020-07-08 18:41:18 -0700

    yuri: update to current config

--- yuri.nix
+++ yuri.nix
@@ -1,191 +1,132 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    ./hardware-configuration.nix
+    ./common.nix
     ./redhat-config.nix
     ./rust.nix
   ];
 
-  nix = {
-    autoOptimiseStore = true;
-    buildCores = 32;
-    useSandbox = true;
-    gc.automatic = true;
-  };
+  boot = {
+    binfmt.registrations."qemu-aarch64" = {
+      interpreter = "${pkgs.qemu}/bin/qemu-aarch64";
+      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'';
+      mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff'';
+    };
 
-  nixpkgs.config = {
-    allowUnfree = true;
-    vim.ftNixSupport = true;
-    #virtualbox.enableExtensionPack = true;
-  };
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  fileSystems = {
-    "/var/lib/docker" = {
-      device = "/dev/disk/by-label/ROOT";
-      fsType = "btrfs";
-      options = [ "subvol=@docker" ];
+    loader = {
+      grub.enable              = false;
+      systemd-boot.enable      = true;
+      efi.canTouchEfiVariables = true;
     };
   };
 
   networking = {
-    networkmanager.enable = true;
     hostName = "yuri";
-    firewall.allowedTCPPorts = [ 12345 ];
-    #firewall.allowedUDPPorts = [ 67 68 ];
+
+    firewall = {
+      allowedTCPPorts   = [ 5201 16509 ];
+      trustedInterfaces = [ "vnet0" "vnet1" ];
+    };
+
+    networkmanager = {
+      dns    = "dnsmasq";
+      enable = true;
+    };
   };
 
   hardware = {
     enableAllFirmware = true;
+
     bluetooth = {
-      enable = true;
+      enable      = true;
       powerOnBoot = false;
     };
 
     pulseaudio = {
-      enable = true;
+      enable  = true;
       package = pkgs.pulseaudioFull;
     };
   };
 
-  i18n = {
-    consoleFont   = "Lat2-Terminus16";
-    consoleKeyMap = "dvorak";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "US/Pacific";
-  #time.timeZone = "America/New_York";
-  #time.timeZone = "Europe/Berlin";
-
-  security.sudo.wheelNeedsPassword = false;
-
-  environment = {
-    #etc."binfmt.d/qemu-aarch64.conf".text = ":qemu-aarch64:M::\\x7fELF\\x02\\x01\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x02\\x00\\xb7:\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xfe\\xff\\xff:/usr/bin/qemu-aarch64:\n";
-
-    shells = [ "${pkgs.zsh}/bin/zsh" ];
-
-    sessionVariables = {
-      EDITOR = "vim";
-    };
-
-    systemPackages = with pkgs; [
-      (import ./vim-config.nix)
-
-      gnupg
-      vim
-      vimPlugins.vundle
-      xorg.xmodmap
-    ];
-  };
-
   programs = {
-    tmux = {
-      enable = true;
-      clock24 = true;
-      keyMode = "vi";
-      historyLimit = 50000;
-      terminal = "screen-256color";
-      extraTmuxConf = ''
-        set-option -g status-bg black
-        set-option -g status-fg white
-        set-window-option -g window-status-current-bg black
-        set-window-option -g window-status-current-fg cyan
-      '';
-    };
-
-    zsh = {
-      enable = true;
-      interactiveShellInit = ''
-        cat << EOF > $HOME/.zshrc
-        source ${import ./zsh-config.nix}
-        EOF
-      '';
-    };
+    gnupg.agent.enable = true;
+    zsh.promptColor    = "cyan";
   };
 
   services = {
+    btrfs.autoScrub.enable = true;
+    fwupd.enable           = true;
+
     xserver = {
-      enable     = true;
-      layout     = "us";
-      xkbVariant = "dvp";
-      xkbOptions = "terminate:ctrl_alt_bksp, ctrl:nocaps";
+      layout       = "us";
+      videoDrivers = [ "nvidia" ];
+      xkbOptions   = "terminate:ctrl_alt_bksp, ctrl:nocaps";
+      xkbVariant   = "dvp";
 
       desktopManager.gnome3.enable = true;
-      desktopManager.xterm.enable = false;
+      desktopManager.xterm.enable  = false;
 
-      displayManager.gdm.enable = true;
-      videoDrivers = [ "nvidia" ];
+      displayManager.gdm = {
+        autoSuspend = false;
+        enable      = true;
+        wayland     = false;
+      };
     };
-
-    udev.extraRules = ''
-      ATTRS{idVendor}=="2544", ATTRS{idProduct}=="0002", MODE="666"
-      ATTRS{idVendor}=="1366", ATTRS{idProduct}=="1015", GROUP="wheel", MODE="666", TAG+="uaccess"
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0407", GROUP="wheel", TAG+="uaccess"
-      SUBSYSTEM=="usb", ACTION=="add|change", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0407", GROUP="wheel", TAG+="uaccess"
-    '';
 
     openssh = {
-      enable = true;
+      enable                 = true;
       passwordAuthentication = false;
-      permitRootLogin = "no";
+      permitRootLogin        = "no";
     };
-
-    sshguard.enable = true;
 
     avahi = {
       enable   = true;
       nssmdns  = true;
     };
 
-    printing.enable = true;
+    printing = {
+      enable  = true;
+      drivers = [ pkgs.gutenprint pkgs.hplip ];
+    };
 
     tcsd.enable = true;
   };
 
-  #system.autoUpgrade.enable = true;
-
   virtualisation = {
     docker = {
       autoPrune.enable = true;
-      enable = true;
-      storageDriver = "btrfs";
-    };
-
-    rkt = {
-      enable = true;
-      gc.automatic = true;
+      enable           = true;
+      storageDriver    = "btrfs";
     };
 
     libvirtd = {
-      enable = true;
-      onShutdown = "shutdown";
+      enable       = true;
+      extraOptions = [ "--listen" ];
+      onShutdown   = "shutdown";
+
+      extraConfig = ''
+        listen_tls = 0
+        listen_tcp = 1
+        auth_tcp = "none"
+      '';
     };
-
-    #virtualbox.host.enable = true;
   };
 
-  users.extraUsers.alex = {
-    name        = "alex";
-    group       = "users";
-    extraGroups = [ "wheel" "plugdev" "rkt" "libvirtd" ];
-    createHome  = true;
-    home        = "/home/alex";
-    shell       = "/run/current-system/sw/bin/zsh";
+  users.extraUsers = {
+    aaron.shell      = "${pkgs.bashInteractive}/bin/bash";
+    adahiya          = {};
+    alex.extraGroups = [ "wheel" "plugdev" "libvirtd" ];
   };
 
-  users.extraUsers.rkt = {
-    name        = "rkt";
-    group       = "rkt";
-    createHome  = false;
+  system = {
+    stateVersion = "20.03";
+
+    activationScripts.lib64 = ''
+      echo "setting up /lib64..."
+      mkdir -p /lib64
+      ln -sfT ${pkgs.stdenv.glibc}/lib/ld-linux-x86-64.so.2 /lib64/.ld-linux-x86-64.so.2
+      mv -Tf /lib64/.ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2
+    '';
   };
 }
