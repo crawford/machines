d68aa361c9de26253f5bc3189db2fb84f721a686
Alex Crawford <machines@code.acrawford.com>
2020-07-11 19:53:30 -0700

    tenerife: add initial config

---
+++ tenerife.nix
@@ -0,0 +1,81 @@
+{ config, lib, pkgs, ... }:
+
+let cfg = config.tenerife;
+in
+{
+  imports = [ ./common.nix ];
+
+  options.tenerife = {
+    domain = lib.mkOption {
+      description = ''
+        The domain of the site.
+      '';
+    };
+
+    gateway = lib.mkOption {
+      description = ''
+        The IP address of the gateway.
+      '';
+    };
+
+    ipAddress = lib.mkOption {
+      description = ''
+        The IP address of the machine.
+      '';
+    };
+
+    ipAddressPrefix = lib.mkOption {
+      description = ''
+        The CIDR prefix length of address of the machine.
+      '';
+      type = lib.types.ints.between 0 32;
+    };
+
+    nameservers = lib.mkOption {
+      description = ''
+        The list of nameservers to use.
+      '';
+    };
+  };
+
+  config = {
+    boot.loader = {
+     efi.canTouchEfiVariables = true;
+     systemd-boot.enable      = true;
+     timeout                  = 1;
+    };
+
+    networking = {
+      domain      = "${cfg.domain}";
+      hostName    = "tenerife";
+      nameservers = cfg.nameservers;
+      useDHCP     = false;
+
+      defaultGateway = {
+        address   = "${cfg.gateway}";
+        interface = "eno1";
+      };
+
+      interfaces.eno1.ipv4.addresses = [{
+        address      = "${cfg.ipAddress}";
+        prefixLength = cfg.ipAddressPrefix;
+      }];
+    };
+
+    programs.zsh.promptColor = "yellow";
+
+    services = {
+      btrfs.autoScrub.enable = true;
+      fwupd.enable           = true;
+    };
+
+    system = {
+      stateVersion = "20.03";
+
+      autoUpgrade = {
+        allowReboot = true;
+        enable      = true;
+      };
+    };
+  };
+}
