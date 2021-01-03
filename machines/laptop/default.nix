b7e56efe86e742a4675106cbd5ad7f463c23e408
Alex Crawford <alex@acrawford.com>
2021-01-03 15:45:18 -0800

    laptops: refactor common bits
    This also universally adds "alex" to the "plugdev" group, removes some
    module parameteres from deepwater, and enables all firmware. I wonder if
    these last two have been causing the Wi-Fi on deepwater to be slow.

--- machines/laptop/default.nix
+++ machines/laptop/default.nix
@@ -1,3 +1,24 @@
+{ pkgs, ...}:
+
 {
   imports = [ ../. ];
+
+  boot.loader = {
+    grub.enable              = false;
+    systemd-boot.enable      = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  hardware = {
+    bluetooth.enable   = true;
+    enableAllFirmware  = true;
+
+    pulseaudio = {
+      enable  = true;
+      package = pkgs.pulseaudioFull;
+    };
+  };
+
+  networking.networkmanager.enable = true;
+  powerManagement.powertop.enable  = true;
 }
