db9a800afac298d20d465a92d46efba8d783c5f3
Alex Crawford <alex@acrawford.com>
2021-02-07 02:45:59 -0800

    pepcon: convert from a virtual to physical machine
    "I'm a real boy!"

--- pepcon.nix
+++ pepcon.nix
@@ -2,14 +2,21 @@
 
 {
   imports = [
+    <nixos-hardware/common/cpu/intel>
+    <nixos-hardware/common/pc>
+    <nixos-hardware/common/pc/ssd>
     ./.
-    modules/awesome.nix
+    modules/btrfs.nix
+    modules/gnome.nix
     modules/rust.nix
+    modules/server.nix
   ];
 
-  boot.loader = {
-    systemd-boot.enable      = true;
-    efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    device      = "/dev/sda";
+    enable      = true;
+    useOSProber = true;
+    version     = 2;
   };
 
   environment = {
@@ -19,17 +26,13 @@
       libusb
       pkg-config
     ];
-
-    variables = {
-      GTK_THEME = "Adwaita-dark";
-    };
   };
 
   networking = {
     firewall.extraCommands = "iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE";
     hostName               = "pepcon";
 
-    interfaces.enp0s8.ipv4.addresses = [{
+    interfaces.enp7s0.ipv4.addresses = [{
       address = "192.168.0.1";
       prefixLength = 24;
     }];
@@ -37,22 +40,13 @@
 
   programs.zsh.promptColor = "#ff8700";
 
-  services = {
-    udev.extraRules = ''
-      ATTR{idVendor}=="1366", ATTR{idProduct}=="1010", MODE="0666"
-      ATTR{idVendor}=="1366", ATTR{idProduct}=="1015", MODE="0666"
-      ATTR{idVendor}=="0483", ATTR{idProduct}=="374b", MODE="0666"
-      ATTR{idVendor}=="21a9", ATTR{idProduct}=="1005", MODE="0666"
-      KERNEL=="hidraw*", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f001", MODE="0666"
-    '';
-
-    xserver.displayManager.autoLogin = {
-      enable = true;
-      user   = "alex";
-    };
-  };
+  services.udev.extraRules = ''
+    ATTR{idVendor}=="1366", ATTR{idProduct}=="1010", MODE="0666"
+    ATTR{idVendor}=="1366", ATTR{idProduct}=="1015", MODE="0666"
+    ATTR{idVendor}=="0483", ATTR{idProduct}=="374b", MODE="0666"
+    ATTR{idVendor}=="21a9", ATTR{idProduct}=="1005", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f001", MODE="0666"
+  '';
 
   system.stateVersion = "20.09";
-
-  virtualisation.virtualbox.guest.enable = true;
 }
