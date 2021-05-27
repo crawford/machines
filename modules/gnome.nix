fedd04785d4a1ebc996d8a61aafd6220fdd67af7
Alex Crawford <alex@acrawford.com>
2021-05-26 19:59:20 -0700

    gnome: update key names

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,12 +1,12 @@
 {
   services = {
-    gnome3.sushi.enable = true;
+    gnome.sushi.enable = true;
 
     xserver = {
       enable = true;
 
       displayManager.gdm.enable    = true;
-      desktopManager.gnome3.enable = true;
+      desktopManager.gnome.enable = true;
     };
   };
 }
