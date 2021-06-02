d866ffce7ab4662610a3709cced484db205af3d8
Alex Crawford <alex@acrawford.com>
2021-06-02 16:14:25 -0700

    laptop: various improvements
    - Enable thinkfan
    - Enable graphics acceleration (supposedly)
    - Enable acpid

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -5,14 +5,22 @@
     ./printer.nix
   ];
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub.enable              = false;
-    systemd-boot.enable      = true;
+  boot = {
+    extraModprobeConfig = ''
+      options thinkpad_acpi fan_control=1
+    '';
+
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub.enable              = false;
+      systemd-boot.enable      = true;
+    };
   };
 
   hardware = {
     bluetooth.enable = true;
+
+    opengl.extraPackages = [ pkgs.vaapiIntel ];
 
     pulseaudio = {
       enable  = true;
@@ -22,5 +30,7 @@
 
   networking.networkmanager.enable = true;
   powerManagement.powertop.enable  = true;
+  services.acpid.enable            = true;
   services.fwupd.enable            = true;
+  services.thinkfan.enable         = true;
 }
