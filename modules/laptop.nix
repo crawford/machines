57e4bea28bf3254865b5bd65fcf0800b89c70978
Alex Crawford <alex@acrawford.com>
2021-06-07 21:38:00 -0700

    laptop: lower the fan's temperature thresholds
    Let's see if this helps prevent the fan from having to spin up extra
    high. Adapted from Rok's dotfiles [1].
    
    [1]: https://github.com/garbas/dotfiles/commit/6e2272d04be0ce064b38c89b2623fa876a06f271

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -32,5 +32,17 @@
   powerManagement.powertop.enable  = true;
   services.acpid.enable            = true;
   services.fwupd.enable            = true;
-  services.thinkfan.enable         = true;
+
+  services.thinkfan = {
+    enable = true;
+    levels = [
+      [ 0  0    45 ]
+      [ 1 40    60 ]
+      [ 2 45    65 ]
+      [ 3 50    75 ]
+      [ 4 55    80 ]
+      [ 5 60    85 ]
+      [ 7 65 32767 ]
+    ];
+  };
 }
