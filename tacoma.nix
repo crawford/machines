c808712d68d2c299433fe1785a16856f23498ec4
Alex Crawford <machines@code.acrawford.com>
2021-06-20 10:49:35 -0700

    tacoma: disable openntp
    I enabled this back in 2c438bae, hoping that it would be an NTP server.
    It isn't; not out of the box at least. It seems to have broken time
    synchronization locally though.

--- tacoma.nix
+++ tacoma.nix
@@ -161,8 +161,6 @@ in
     programs.zsh.promptColor = "red";
 
     services = {
-      openntpd.enable = true;
-
       doxie-upload = {
         address   = "${cfg.auxIpAddress}";
         root      = "/mnt/valdez/media/Scans";
