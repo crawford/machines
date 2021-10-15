f634cacdb04a5efccc43513a8c3b12f520dc5a7d
Alex Crawford <machines@code.acrawford.com>
2021-10-15 14:30:26 -0700

    tenerife: migrate to virtual machine

--- tenerife.nix
+++ tenerife.nix
@@ -1,73 +1,14 @@
 { config, lib, ... }:
 
-let cfg = config.tenerife;
-in
 {
   imports = [
-    <nixos-hardware/common/cpu/intel>
-    <nixos-hardware/common/pc>
-    <nixos-hardware/common/pc/hdd>
-    modules/btrfs.nix
     modules/common.nix
     modules/matrix.nix
     modules/server.nix
   ];
 
-  options.tenerife = {
-    domain = lib.mkOption {
-      description = ''
-        The domain of the site.
-      '';
-    };
-
-    gateway = lib.mkOption {
-      description = ''
-        The IP address of the gateway.
-      '';
-    };
-
-    ipAddress = lib.mkOption {
-      description = ''
-        The IP address of the machine.
-      '';
-    };
-
-    ipAddressPrefix = lib.mkOption {
-      description = ''
-        The CIDR prefix length of address of the machine.
-      '';
-      type = lib.types.ints.between 0 32;
-    };
-
-    nameservers = lib.mkOption {
-      description = ''
-        The list of nameservers to use.
-      '';
-    };
-  };
-
   config = {
-    boot.loader = {
-     efi.canTouchEfiVariables = true;
-     systemd-boot.enable      = true;
-    };
-
-    networking = {
-      domain      = cfg.domain;
-      hostName    = "tenerife";
-      nameservers = cfg.nameservers;
-      useDHCP     = false;
-
-      defaultGateway = {
-        address   = cfg.gateway;
-        interface = "eno1";
-      };
-
-      interfaces.eno1.ipv4.addresses = [{
-        address      = cfg.ipAddress;
-        prefixLength = cfg.ipAddressPrefix;
-      }];
-    };
+    networking.hostName = "tenerife";
 
     programs.zsh.promptColor = "yellow";
 
@@ -75,13 +16,9 @@ in
       "/".return = "301 https://www.${config.networking.domain}$request_uri";
     };
 
-    system = {
-      stateVersion = "20.03";
-
-      autoUpgrade = {
-        allowReboot = true;
-        enable      = true;
-      };
+    system.autoUpgrade = {
+      allowReboot = true;
+      enable      = true;
     };
   };
 }
