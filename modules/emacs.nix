a673ab7f674356b96bcd878cda192937e1ad71ec
Alex Crawford <machines@code.acrawford.com>
2021-12-18 17:55:17 -0800

    emacs,pepcon: add emacs
    Trying something new.

---
+++ modules/emacs.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchGit {
+      url = "https://github.com/nix-community/emacs-overlay.git";
+      ref = "master";
+      rev = "80685d6b449905d6fa258eb4b13875327741dc02";
+    }))
+  ];
+
+  programs.vim.defaultEditor = lib.mkForce false;
+
+  services.emacs = {
+    enable        = true;
+    defaultEditor = true;
+    package       = pkgs.emacsPgtkGcc;
+  };
+}
