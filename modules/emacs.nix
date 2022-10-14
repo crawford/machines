825ec2468c7a5c7ab07472cb643d37fe9225d8a2
Alex Crawford <machines@code.acrawford.com>
2022-10-13 18:16:14 -0700

    emacs: switch to stock emacs
    I'm tired of compiling. Plus, I'm not using anything specific to the
    prior variant.

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,14 +1,6 @@
 { lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (import (builtins.fetchGit {
-      url = "https://github.com/nix-community/emacs-overlay.git";
-      ref = "master";
-      rev = "80685d6b449905d6fa258eb4b13875327741dc02";
-    }))
-  ];
-
   environment.systemPackages = [ pkgs.ispell ];
 
   programs.vim.defaultEditor = lib.mkForce false;
@@ -17,11 +9,11 @@
     enable        = true;
     defaultEditor = true;
 
-    package = (pkgs.emacsPackagesFor (pkgs.emacsPgtkGcc.overrideAttrs (attrs: {
-      postInstall = (attrs.postInstall or "") + ''
+    package = (pkgs.emacs.overrideAttrs (self: {
+      postInstall = (self.postInstall or "") + ''
         rm $out/share/applications/emacs.desktop
       '';
-    }))).emacsWithPackages (epkgs: (with epkgs; [
+    })).pkgs.withPackages (epkgs: (with epkgs; [
       nix-mode
       rust-mode
     ]));
