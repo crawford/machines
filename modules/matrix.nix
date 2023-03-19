e18119c3f07b09c172074c0f982c0605486e9677
Alex Crawford <machines@code.acrawford.com>
2023-03-19 11:41:52 -0700

    matrix: fix firewall settings for coturn
    The default listening port was being blocked by the firewall and TCP
    isn't required for the UDP relay port range. This also fixes the
    coturn configuration to disallow internal IPs, as recommended by the
    Matrix documentation, and removes the unrecognized `no-loopback-peers`
    option.

--- modules/matrix.nix
+++ modules/matrix.nix
@@ -15,6 +15,8 @@ in
       to   = coturn.max-port;
     };
     coturnListeningPorts = [
+      coturn.listening-port
+      coturn.alt-listening-port
       coturn.tls-listening-port
       coturn.alt-tls-listening-port
     ];
@@ -27,7 +29,6 @@ in
       ];
 
       allowedUDPPortRanges = [ coturnRange ];
-      allowedTCPPortRanges = [ coturnRange ];
     };
 
   security.acme = {
@@ -60,8 +61,19 @@ in
 
       extraConfig = ''
         cipher-list="HIGH"
-        no-loopback-peers
         no-multicast-peers
+
+        denied-peer-ip=0.0.0.0-0.255.255.255
+        denied-peer-ip=100.64.0.0-100.127.255.255
+        denied-peer-ip=127.0.0.0-127.255.255.255
+        denied-peer-ip=169.254.0.0-169.254.255.255
+        denied-peer-ip=192.0.0.0-192.0.0.255
+        denied-peer-ip=192.0.2.0-192.0.2.255
+        denied-peer-ip=192.88.99.0-192.88.99.255
+        denied-peer-ip=198.18.0.0-198.19.255.255
+        denied-peer-ip=198.51.100.0-198.51.100.255
+        denied-peer-ip=203.0.113.0-203.0.113.255
+        denied-peer-ip=240.0.0.0-255.255.255.255
       '';
     };
 
