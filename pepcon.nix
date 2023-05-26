eaed44586f51b4d58e7eaafc5a1f79ca4f4bf8e6
Alex Crawford <machines@code.acrawford.com>
2023-05-25 19:30:52 -0700

    pepcon: turn on and off the office lights with suspend
    Let's see how annoying this is.

--- pepcon.nix
+++ pepcon.nix
@@ -50,6 +50,28 @@
     KERNEL=="hidraw*", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f001", MODE="0666"
   '';
 
+  systemd.services.crawfice-lights = {
+    description = "Turns on and off the lights";
+    wantedBy = [ "sleep.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      User = "alex";
+      EnvironmentFile = "/home/alex/.config/crawfice-lights/env";
+      ExecStart = "${pkgs.home-assistant-cli}/bin/hass-cli state turn_off light.crawfice";
+      ExecStop = [
+        "/bin/sh -c 'SERVER=$(basename $HASS_SERVER); while ! ${pkgs.iputils}/bin/ping -c 1 $SERVER > /dev/null 2>&1; do sleep 0.5; done'"
+        "${pkgs.home-assistant-cli}/bin/hass-cli state turn_on light.crawfice"
+      ];
+    };
+
+    unitConfig = {
+      Before = [ "sleep.target" ];
+      StopWhenUnneeded = "yes";
+    };
+  };
+
   users.extraUsers.alex.extraGroups = [ "libvirtd" ];
 
   virtualisation = {
