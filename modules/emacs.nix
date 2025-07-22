c43032383f699066a143499a2ceeff95b2b523a5
Alex Crawford <machines@code.acrawford.com>
2025-07-22 14:15:35 -0700

    Remove unneeded override in emacs config

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -31,27 +31,6 @@
         swift-mode
         yaml-mode
       ];
-
-      overrides = self: super: rec {
-        # Taken from https://github.com/magit/magit/issues/5011#issuecomment-1838598138
-        seq = self.callPackage ({ elpaBuild, fetchurl, lib }:
-          elpaBuild rec {
-            pname = "seq";
-            ename = "seq";
-            version = "2.24";
-            src = fetchurl {
-              url = "https://elpa.gnu.org/packages/seq-2.24.tar";
-              sha256 = "1w2cysad3qwnzdabhq9xipbslsjm528fcxkwnslhlkh8v07karml";
-            };
-            packageRequires = [];
-            meta = {
-              homepage = "https://elpa.gnu.org/packages/seq.html";
-              license = lib.licenses.free;
-            };
-            # tests take a _long_ time to byte-compile, skip them
-            postInstall = ''rm -r $out/share/emacs/site-lisp/elpa/${pname}-${version}/tests'';
-          }) {};
-      };
     };
   };
 }
